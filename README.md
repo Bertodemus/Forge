817391af802b3cc074671cdfd02c5a82b8f2dcbc
Bertodemus <roberto@robertorubet.com>
2020-10-17 08:09:10 -0400

    altered base logic; able to generate a simple readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Forge
 App for generating Readme Files
+<p>Berto</p>
+<p>place</p>
+<p>Person</p>
+<p>www</p>
+<p>www</p>
--- index.js
+++ index.js
@@ -1,16 +1,57 @@
+//Establish required modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+let readParam = "";
+
+
 // array of questions for user
-const questions = [
+inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Where do you live?",
+        name: "location"
+      },
+      {
+        type: "input",
+        message: "Tell me about yourself",
+        name: "bio"
+      },
+      {
+        type: "input",
+        message: "What is your linkedIn URL?",
+        name: "linkedin"
+      },
+      {
+        type: "input",
+        message: "What is your github URL?",
+        name: "github"
+      },
+  ])
+  .then(function(response) {
+    console.log(response);
+    // const readParam = response;
+    // console.log(readParam);
 
-];
+    readParam =
+`# Forge
+App for generating Readme Files
+<p>${response.name}</p>
+<p>${response.location}</p>
+<p>${response.bio}</p>
+<p>${response.linkedin}</p>
+<p>${response.github}</p>`
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+    fs.writeFile("readme.md",readParam, function(err){
+        if (err) {
+            throw err;
+        }
+    });
+    
+  });
